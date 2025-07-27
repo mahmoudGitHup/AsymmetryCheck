3269eed8b8c517d2b1c8712a7b545363e5286c64
mahmoudGitHup <136239937+mahmoudGitHup@users.noreply.github.com>
2025-07-27 17:22:11 +0300

    Update project.py

--- project.py
+++ project.py
@@ -62,7 +62,7 @@ val_test_transform = transforms.Compose([
 ])
 
 model = EmotionCNN()
-    model.load_state_dict(torch.load("model/face_classification_model.pth", map_location=torch.device('cpu')))
+model.load_state_dict(torch.load("model/face_classification_model.pth", map_location=torch.device('cpu')))
 
 model.eval()
 
